d2bb1c3b8e049a95a5fde6dc28f81d55f4aa744a
aditiyakusuma <tariganaditya779@gmail.com>
2026-08-09 16:27:41 +0700

    Force HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Models\Setting;
 use Illuminate\Support\Facades\View;
 
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+         if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+        }
+        
         View::composer('*', function ($view) {
 
             $view->with('setting', Setting::first());
